bb1e611d8b25b0eac91b8e736149e8f9c2a9ae34
Anna Luíza <aniz1987@hotmail.com>
2014-02-05 00:48:31 -0300

    incluindo view e modelos

--- src/generator/AnnotationGetter.java
+++ src/generator/AnnotationGetter.java
@@ -3,13 +3,15 @@ package generator;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
+import annotations.BoolColumn;
 import annotations.DateColumn;
 import annotations.IntColumn;
 import annotations.LongColumn;
 import annotations.Table;
+import annotations.TypeColumn;
 import annotations.VarcharColumn;
 
-public class AnnotationGetter {
+public class AnnotationGetter{
 	
 	public static ArrayList<VarcharColumn> getVarcharColumnAnnotations(Class<?> T){
 		ArrayList<VarcharColumn> annotations = new ArrayList<VarcharColumn>();
@@ -50,7 +52,27 @@ public class AnnotationGetter {
 		}
 		return annotations;
 	}
-		
+	
+	public static ArrayList<TypeColumn> getTypeColumnAnnotations(Class<?> T){
+		ArrayList<TypeColumn> annotations = new ArrayList<TypeColumn>();
+		for (Field field : T.getDeclaredFields()) {
+			if(field.isAnnotationPresent(TypeColumn.class)){
+				annotations.add(field.getAnnotation(TypeColumn.class));
+			}
+		}
+		return annotations;
+	}
+	
+	public static ArrayList<BoolColumn> getBoolColumnAnnotations(Class<?> T){
+		ArrayList<BoolColumn> annotations = new ArrayList<BoolColumn>();
+		for (Field field : T.getDeclaredFields()) {
+			if(field.isAnnotationPresent(BoolColumn.class)){
+				annotations.add(field.getAnnotation(BoolColumn.class));
+			}
+		}
+		return annotations;
+	}
+	
 	public static Table getTableAnnotation(Class<?> T){
 		return T.getAnnotation(Table.class);
 	}
