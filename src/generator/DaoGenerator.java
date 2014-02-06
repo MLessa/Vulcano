9bca702956ca3a95fa45202752932853989ac785
Anna Luíza <aniz1987@hotmail.com>
2014-02-06 01:06:10 -0300

    aspas

--- src/generator/DaoGenerator.java
+++ src/generator/DaoGenerator.java
@@ -36,6 +36,7 @@ public class DaoGenerator {
         String select;
         String update;
         String where = "";
+        String condicional = "";
         
         String a = "";
         String b = T.getName().split("model.")[1].toLowerCase();
@@ -62,78 +63,85 @@ public class DaoGenerator {
             valuesSelect = "";
             valuesUpdate = "";
             where = "";
+            condicional = "";
             for (VarcharColumn varV : varcharList) {
                 if (!varV.name().equals("id")) {
                 	insert += varV.name()+",";
                 	a = varV.name().valueOf(varV.name().toCharArray()[0]).toUpperCase() + varV.name().substring(1);
-                    valuesInsert += b+".get"+a+"(),";
-                    valuesSelect += b+".set"+a+"(rs.getString("+ '"'+ varV.name() + '"'+"); ";
-                    valuesUpdate += varV.name() + "="+b+".get"+a+"(),";
+                	valuesInsert += "+"+b+".get"+a+"()+"+'"'+","+'"';
+                    valuesSelect += b+".set"+a+"(rs.getString("+ '"'+ varV.name() + '"'+")); ";
+                    valuesUpdate += varV.name() + "="+'"'+"+"+b+".get"+a+"()"+"+"+'"'+",";
+                    condicional += "if(exemple.get"+a+"()"+"!= null) query+=" + '"' + "and " + varV.name() + "="+'"'+"+"+ "exemple.get" + a + "();" ;
                 }else{
                 	a = varV.name().valueOf(varV.name().toCharArray()[0]).toUpperCase() + varV.name().substring(1);   
-                    where = varV.name() + "="+b+".get"+a+"()";
+                    where = varV.name() + "="+'"'+"+"+b+".get"+a+"()";
                 }
             }
             for (LongColumn varL : longList) {
                 if (!varL.name().equals("id")) {
                     insert += varL.name()+",";
                     a = varL.name().valueOf(varL.name().toCharArray()[0]).toUpperCase() + varL.name().substring(1);
-                    valuesInsert += b+".get"+a+"(),";
-                    valuesSelect += b+".set"+a+"(rs.getString("+ '"'+ varL.name() + '"'+"); ";
-                    valuesUpdate += varL.name() + "="+b+".get"+a+"(),";
+                	valuesInsert += "+"+b+".get"+a+"()+"+'"'+","+'"';
+                    valuesSelect += b+".set"+a+"(rs.getString("+ '"'+ varL.name() + '"'+")); ";
+                    valuesUpdate += varL.name() + "="+'"'+"+"+b+".get"+a+"()"+"+"+'"'+",";
+                    condicional += "if(exemple.get"+a+"()"+"!= null) query+=" + '"' + "and " + varL.name() + "="+'"'+"+"+ "exemple.get" + a + "();" ;   
                 }
                 else{
                 	a = varL.name().valueOf(varL.name().toCharArray()[0]).toUpperCase() + varL.name().substring(1);   
-                    where = varL.name() + "="+b+".get"+a+"()";
+                    where = varL.name() + "="+'"'+"+"+b+".get"+a+"()";
                 }
             }
             for (DateColumn varD : dateList) {
                 if (!varD.name().equals("id")) {
                     insert += varD.name()+",";
                     a = varD.name().valueOf(varD.name().toCharArray()[0]).toUpperCase() + varD.name().substring(1);
-                    valuesInsert += b+".get"+a+"(),";
-                    valuesSelect += b+".set"+a+"(rs.getDate("+ '"'+ varD.name() + '"'+"); ";
-                    valuesUpdate += varD.name() + "="+b+".get"+a+"(),";
+                	valuesInsert += "+"+b+".get"+a+"()+"+'"'+","+'"';
+                    valuesSelect += b+".set"+a+"(rs.getDate("+ '"'+ varD.name() + '"'+")); ";
+                    valuesUpdate += varD.name() + "="+'"'+"+"+b+".get"+a+"()"+"+"+'"'+",";
+                    condicional += "if(exemple.get"+a+"()"+"!= null) query+=" + '"' + "and " + varD.name() + "="+'"'+"+"+ "exemple.get" + a + "();" ;
                 }
                 else{
                 	a = varD.name().valueOf(varD.name().toCharArray()[0]).toUpperCase() + varD.name().substring(1);   
-                    where = varD.name() + "="+b+".get"+a+"()";
+                    where = varD.name() + "="+'"'+"+"+b+".get"+a+"()";
                 }
             }
             for (BoolColumn varB : boolList) {
                 if (!varB.name().equals("id")) {
                     insert += varB.name()+",";
                     a = varB.name().valueOf(varB.name().toCharArray()[0]).toUpperCase() + varB.name().substring(1);
-                    valuesInsert += b+".get"+a+"(),";
-                    valuesSelect += b+".set"+a+"(rs.getInt("+ '"'+ varB.name() +'"'+"); ";
-                    valuesUpdate += varB.name() + "="+b+".get"+a+"(),";
+                	valuesInsert += "+"+b+".get"+a+"()+"+'"'+","+'"';
+                    valuesSelect += b+".set"+a+"(rs.getInt("+ '"'+ varB.name() +'"'+")); ";
+                    valuesUpdate += varB.name() + "="+'"'+"+"+b+".get"+a+"()"+"+"+'"'+",";
+                    condicional += "if(exemple.get"+a+"()"+"!= null) query+=" + '"' + "and " + varB.name() + "="+'"'+"+"+ "exemple.get" + a + "();" ;
                 }
                 else{
                 	a = varB.name().valueOf(varB.name().toCharArray()[0]).toUpperCase() + varB.name().substring(1);   
-                    where = varB.name() + "="+b+".get"+a+"()";
+                    where = varB.name() + "="+'"'+"+"+b+".get"+a+"()";
                 }
             }
             for (IntColumn varI :intList) {
                 if (!varI.name().equals("id")) {    
                 insert += varI.name()+",";
                     a = varI.name().valueOf(varI.name().toCharArray()[0]).toUpperCase() + varI.name().substring(1);
-                    valuesInsert += b+".get"+a+"(),";
-                    valuesSelect += b+".set"+a+"(rs.getInt("+ '"'+ varI.name() +'"'+"); ";
-                    valuesUpdate += varI.name() + "="+b+".get"+a+"(),";
+                	valuesInsert += "+"+b+".get"+a+"()+"+'"'+","+'"';
+                    valuesSelect += b+".set"+a+"(rs.getInt("+ '"'+ varI.name() +'"'+")); ";
+                    valuesUpdate += varI.name() + "="+'"'+"+"+b+".get"+a+"()"+"+"+'"'+",";
+                    condicional += "if(exemple.get"+a+"()"+"!= null) query+=" + '"' + "and " + varI.name() + "="+'"'+"+"+ "exemple.get" + a + "();" ;
                 }
                 else{
                 	a = varI.name().valueOf(varI.name().toCharArray()[0]).toUpperCase() + varI.name().substring(1);   
-                    where = varI.name() + "="+b+".get"+a+"()";
+                    where = varI.name() + "="+'"'+"+"+b+".get"+a+"()";
                 }
             }
-            valuesInsert=valuesInsert.substring(0, valuesInsert.length()-1);
+            valuesInsert=valuesInsert.substring(0, valuesInsert.length()-3);
             insert=insert.substring(0, insert.length()-1);
             valuesUpdate = valuesUpdate.substring(0, valuesUpdate.length()-1);
             valuesUpdate += " where "+where;
-            fileString=fileString.replaceAll("<InsertFildsValues>", T.getName().split("model.")[1].toLowerCase());
-            fileString=fileString.replaceAll("<InsertFields>", T.getName().split("model.")[1].toLowerCase());
+            fileString=fileString.replaceAll("<InsertFildsValues>", valuesInsert);
+            fileString=fileString.replaceAll("<InsertFields>", insert);
             fileString=fileString.replaceAll("<UpdateFields>", valuesUpdate);
             fileString=fileString.replaceAll("<SelectFildsValues>", valuesSelect);
+            fileString=fileString.replaceAll("<condicional>", condicional);
 
             file = new File("src/dao/"+T.getName().split("model.")[1]+"Dao.java");
             FileOutputStream fop = new FileOutputStream(file);
